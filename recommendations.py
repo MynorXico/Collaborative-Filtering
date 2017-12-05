22e77a911d1bfefb2130198fd3c0d0e2f11a301e
Mynor Xico <mynorottoniel.xico2@gmail.com>
2017-12-05 11:04:46 -0600

    Get Recommended People
    Recommended people for going to watch a movie

--- recommendations.py
+++ recommendations.py
@@ -143,4 +143,15 @@ def getRecommendations(prefs, person, similarity=sim_pearson):
     rankings.reverse()
     return rankings
 
-print(getRecommendations(critics, 'Toby', similarity=sim_distance))
+# Transformation
+def transformPrefs(prefs):
+    result = {}
+    for person in prefs:
+        for item in prefs[person]:
+            result.setdefault(item, {})
+
+            # Flip item and person
+            result[item][person] = prefs[person][item]
+    return result
+
+print(getRecommendations(transformPrefs(critics), 'Just My Luck'))
